5c06ba675d818c98dc2a618b90dc52b48b766d1a
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-27 15:28:22 +0200

    properlyUsedDishWasherRunShouldEndWithSuccessStatus test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
@@ -2,10 +2,39 @@ package edu.iis.mto.testreactor.exc4;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class DishWasherTest {
+
+    DirtFilter dirtFilter;
+    Door door;
+    Engine engine;
+    WaterPump waterPump;
+
+    @Before public void setup(){
+        dirtFilter = Mockito.mock(DirtFilter.class);
+        door = Mockito.mock(Door.class);
+        engine = Mockito.mock(Engine.class);
+        waterPump = Mockito.mock(WaterPump.class);
+    }
+
+    @Test public void properlyUsedDishWasherRunShouldEndWithSuccessStatus(){
+        DishWasher dishWasher = new DishWasher(waterPump,engine,dirtFilter,door);
+
+        WashingProgram program = WashingProgram.ECO;
+
+        ProgramConfiguration programConfiguration = ProgramConfiguration.builder().withProgram(program).withTabletsUsed(false).build();
+
+        RunResult actualRunResult = dishWasher.start(programConfiguration);
+        Assert.assertEquals(Status.SUCCESS,actualRunResult.getStatus());
+    }
 
     @Test
     public void test() {
@@ -13,3 +42,5 @@ public class DishWasherTest {
     }
 
 }
+
+
